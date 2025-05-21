1280faa04449776dfd58d9e0dc1aae1c4d693041
francescoluca <francescoluca14@gmail.com>
2025-05-21 19:50:54 +0200

    Login aggiunto

--- src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/authentication/AuthConfiguration.java
@@ -35,36 +35,36 @@ public class AuthConfiguration {
     }
 
     @Bean
-    protected SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
-
-        httpSecurity
-            .csrf().and().cors().disable()
-            .authorizeHttpRequests()
-            // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-            .requestMatchers(HttpMethod.GET, "/", "/index", "/register", "/css/**", "/images/**", "/favicon.ico").permitAll()
-            // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register
-            .requestMatchers(HttpMethod.POST, "/register", "/login").permitAll()
-            .requestMatchers(HttpMethod.GET, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
-            .requestMatchers(HttpMethod.POST, "/admin/**").hasAnyAuthority(ADMIN_ROLE)
-            // tutti gli utenti autenticati possono accere alle pagine rimanenti
-            .anyRequest().authenticated()
-            // LOGIN: qui definiamo il login
-            .and().formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/success", true)
-                .failureUrl("/login?error=true")
-            // LOGOUT: qui definiamo il logout
-            .and().logout()
-                // il logout è attivato con una richiesta GET a "/logout"
-                .logoutUrl("/logout")
-                // in caso di successo, si viene reindirizzati alla home
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .clearAuthentication(true).permitAll();
-
-        return httpSecurity.build();
+    protected SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        http
+          .csrf().and().cors().disable()
+          .authorizeHttpRequests()
+            .requestMatchers(HttpMethod.GET,
+                 "/", "/index", "/login", "/register",
+                 "/css/**", "/images/**", "/favicon.ico")
+               .permitAll()
+            .requestMatchers(HttpMethod.POST, "/register", "/login")
+               .permitAll()
+            .requestMatchers("/admin/**")
+               .hasAuthority(ADMIN_ROLE)
+            .anyRequest()
+               .authenticated()
+          .and()
+            .formLogin()
+              .loginPage("/login")
+              .permitAll()
+              .defaultSuccessUrl("/success", true)
+              .failureUrl("/login?error=true")
+          .and()
+            .logout()
+              .logoutUrl("/logout")
+              .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+              .logoutSuccessUrl("/")
+              .invalidateHttpSession(true)
+              .deleteCookies("JSESSIONID")
+              .clearAuthentication(true)
+              .permitAll();
+        return http.build();
     }
+
 }
