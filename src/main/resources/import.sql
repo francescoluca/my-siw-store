4d3a5392b47f3b2090d5fc26985367312f452945
francescoluca <francescoluca14@gmail.com>
2025-05-25 19:16:48 +0200

    Aggiunti dati in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,11 @@
+insert into television(id, model, brand,release_date,screen_inches) values(999, 'BRAVIA 9', 'Sony',2024, 65);
+insert into television(id, model, brand,release_date,screen_inches) values(998, 'BRAVIA 8 II', 'Sony',2025, 65);
+insert into television(id, model, brand,release_date,screen_inches) values(997, 'BRAVIA 8', 'Sony',2024, 65);
+insert into television(id, model, brand,release_date,screen_inches) values(996, 'BRAVIA 7', 'Sony',2024, 65);
+insert into television(id, model, brand,release_date,screen_inches) values(995, 'BRAVIA 5', 'Sony',2025, 65);
+
+
+
+
 insert into users(id, name, surname, email) values(1, 'paolo', 'paolo', 'paolo@gmail.com');
 insert into credentials(id, password, role, username, user_id) values(1, '$2a$10$F605H0XOC06ODyI.oQnCzeMWpWRPNOH2DM2Lmf.ZSAkNrTG6Kqa3q', 'ADMIN', 'paolo',1);
