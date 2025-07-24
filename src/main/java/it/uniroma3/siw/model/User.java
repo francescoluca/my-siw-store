c1d2b2353edc8170b60266af54af4072cc3d1838
francescoluca <francescoluca14@gmail.com>
2025-07-24 15:41:45 +0200

    Fixes #15 - rimozione classi ridondanti

--- src/main/java/it/uniroma3/siw/model/User.java
+++ src/main/java/it/uniroma3/siw/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -15,8 +16,8 @@ import jakarta.validation.constraints.NotBlank;
 @Table(name = "users")
 public class User {
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
 	@NotBlank
 	private String name;
@@ -26,42 +27,43 @@ public class User {
 	private String email;
 	@OneToOne
 	private Address address;
-	@OneToOne
-	private ShoppingCart shoppingCart;
-	
+	@ManyToMany
+	private List<InventoryItem> cartItems;
+
 	@OneToMany(mappedBy = "user")
 	private List<Order> orders;
-	
+
 	@OneToMany(mappedBy = "user")
 	private List<PickUpRequest> pickUpRequests;
 
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
@@ -72,14 +74,6 @@ public class User {
 
 	public void setAddress(Address address) {
 		this.address = address;
-	}
-
-	public ShoppingCart getShoppingCart() {
-		return shoppingCart;
-	}
-
-	public void setShoppingCart(ShoppingCart shoppingCart) {
-		this.shoppingCart = shoppingCart;
 	}
 
 	public List<PickUpRequest> getPickUpRequests() {
